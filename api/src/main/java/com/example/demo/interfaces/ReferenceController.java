c3912eaad52f329baacd2a6c53fdb25b30dae56a
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-01-05 00:59:15 +0300

    fix ref controller to load all data

--- api/src/main/java/com/example/demo/interfaces/ReferenceController.java
+++ api/src/main/java/com/example/demo/interfaces/ReferenceController.java
@@ -3,15 +3,14 @@ package com.example.demo.interfaces;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-import reactor.core.publisher.Flux;
+
 @RestController
 @RequestMapping(value = {"/ref-data", "/ref-data/"})
 @RequiredArgsConstructor
@@ -20,12 +19,11 @@ public class ReferenceController {
     private final ReactiveMongoTemplate mongoTemplate;
 
     @GetMapping
-    public Mono<ResponseEntity<Object>> getReferenceData() {
-        // Query the "ref-data" collection to fetch a single object
-        Query query = new Query(); // You can add criteria if necessary
-        return mongoTemplate.findOne(query, Object.class, "ref-data")
-                .map(data -> ResponseEntity.ok(data))
-                .defaultIfEmpty(ResponseEntity.notFound().build());
+    public Mono<ResponseEntity<Flux<Object>>> getReferenceData() {
+        Flux<Object> data = mongoTemplate.findAll(Object.class, "ref-data");
+
+        // Return a ResponseEntity wrapping the Flux
+        return Mono.just(ResponseEntity.ok(data));
     }
 
 }
