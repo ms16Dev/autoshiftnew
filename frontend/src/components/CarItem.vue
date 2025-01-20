2d93fc6715b998069bd3cab0b5ecff672b2bcb39
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-01-20 19:02:55 +0300

    navigation to car details route

--- frontend/src/components/CarItem.vue
+++ frontend/src/components/CarItem.vue
@@ -33,6 +33,7 @@ withDefaults(defineProps<{
 
 <template>
   <div class="relative group">
+    <RouterLink :to="{ name: 'Home', params: {id: car.id} }">
     <div class="absolute inset-px shadow-lg bg-white dark:bg-gray-900  group-hover:bg-pink-50"></div>
     <div class="relative flex flex-col overflow-hidden rounded-lg">
       <div class="group flex-col inset-px shadow-md">
@@ -140,6 +141,7 @@ withDefaults(defineProps<{
 
 
     </div>
+    </RouterLink>
   </div>
 
 </template>
