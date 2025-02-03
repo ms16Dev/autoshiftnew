b268c18f8da2a0a8c780fd52a30b2fd4770c0b75
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-02-03 14:48:25 +0300

    logo edits

--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 
+
+
+
 import IconButtonLink from "./IconButtonLink.vue";
+
 
 </script>
 
@@ -8,8 +12,8 @@ import IconButtonLink from "./IconButtonLink.vue";
       <div class="fixed z-50 bottom-0 w-full h-[50px] bg-gradient-to-r from-[#ff80b5] to-[#9089fc] opacity-80
            md:w-[80px] md:h-full md:start-0 md:top-1/2 md:transform md:-translate-y-1/2 md:bg-gradient-to-t
            xl:w-2/3 xl:h-[50px] xl:top-12 xl:start-1/2 xl:transform xl:-translate-x-1/2 xl:rounded-full xl:bg-gradient-to-r">
-        <router-link to="/" class="hidden translate-x-[40px] xl:-translate-y-[25px]  md:block">
-          <img src="../assets/asl.png" alt="Logo" width="150" height="150">
+        <router-link to="/" class="w-fit xl:translate-x-[40px] xl:-translate-y-[15px]  md:block md:-translate-x-[40]">
+          <img src="../assets/asl.png" alt="Logo" width="120" height="120">
         </router-link>
         <div class="flex justify-center h-full md:h-[100px] md:w-full md:flex-col md:absolute md:top-1/2 md:-translate-y-1/2  xl:flex-row xl:h-full xl:justify-center">
           <IconButtonLink :text="'Home'" :icon="['fas', 'fa-home']" target="home" :class="{'bg-pink-600': $route.path === '/'}"/>
