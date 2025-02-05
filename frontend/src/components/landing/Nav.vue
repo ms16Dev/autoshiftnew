ff8f773f9c2cd8c99c5388ceba3893e6d5d90244
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-02-05 03:49:23 +0300

    fix landing header

--- frontend/src/components/landing/Nav.vue
+++ frontend/src/components/landing/Nav.vue
@@ -1,13 +1,16 @@
 <template>
-  <nav id="header" class="fixed w-full z-30 top-0 text-white">
+  <nav id="header" class="fixed w-full z-30 top-0 text-white shadow-lg">
     <div class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 py-2">
       <div class="pl-4 flex items-center ">
 
+        <div class="rounded-full bg-pink-900">
 
         <img src="../../assets/asl.png" alt="Logo" width="100" height="100">
 
+        </div>
 
-        <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl" href="#">
+
+        <a class="toggleColour text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl py-2" href="#">
           AUTOSHIFT
         </a>
       </div>
