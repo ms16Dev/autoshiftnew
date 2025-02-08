dac7e8a3aec72c44713c598867491f9d3345e15d
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-02-08 23:05:02 +0300

    moved clutter html to small components

---
+++ frontend/src/components/car/ChipItemCheckedSm.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+defineProps({
+  option: {
+    type: String,
+    required: true,
+    default: "1.png"
+  },
+});
+
+
+</script>
+
+<template>
+    <button class=" text-gray-400 bg-pink-50 rounded-full m-1 px-3">
+      <i :class="['fas', 'fa-check', 'text-xl text-pink-300 text-center w-8 h-8  me-2']" />
+      <span class=" font-medium">{{ option }}</span>
+    </button>
+</template>
+
+<style scoped>
+
+</style>
