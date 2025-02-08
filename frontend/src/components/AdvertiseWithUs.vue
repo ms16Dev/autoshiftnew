fd8187b3c3d095d43965a8337fe9cf2607fb7f31
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-02-09 01:57:14 +0300

    complete ui in more page

--- frontend/src/components/AdvertiseWithUs.vue
+++ frontend/src/components/AdvertiseWithUs.vue
@@ -1,17 +1,20 @@
 <template>
-  <section class=" relative w-full h-auto mx-auto flex flex-col items-center text-center bg-pink-400 overflow-hidden shadow-lg p-8 rounded-lg">
+  <section class=" relative w-full h-auto mx-auto flex flex-col items-center text-center bg-purple-200 pattern-bg overflow-hidden shadow-lg p-8 rounded-lg opacity-70">
 
-    <!-- Header -->
-    <h2 class="text-3xl font-bold text-gray-50">Advertise With Us 🚀</h2>
-    <p class="text-md text-gray-300 mt-2 max-w-lg">
-      Get **free space** to showcase your business to thousands of potential customers.
-      Take advantage of our platform to increase your visibility and attract new clients!
-    </p>
+    <div class="bg-white opacity-85 p-10">
+      <!-- Header -->
+      <h2 class="text-3xl font-bold text-gray-800">Advertise With Us 🚀</h2>
+      <p class="text-md text-gray-600 mt-2 max-w-lg text-lg">
+        Get **free space** to showcase your business to thousands of potential customers.
+        Take advantage of our platform to increase your visibility and attract new clients!
+      </p>
 
-    <!-- CTA Button -->
-    <button class="mt-6 bg-white hover:bg-gray-200 text-gray-900 font-bold py-3 px-6 rounded-lg transition shadow-md">
-      Get Your Free Ad Space 📢
-    </button>
+      <!-- CTA Button -->
+      <button class="mt-6 bg-white hover:bg-gray-200 text-gray-900 font-bold py-3 px-6 rounded-lg transition shadow-md">
+        Get Your Free Ad Space 📢
+      </button>
+    </div>
+
 
   </section>
 </template>
@@ -24,3 +27,5 @@
   background-position: center;
 }
 </style>
+<script setup lang="ts">
+</script>
--- frontend/src/views/More.vue
+++ frontend/src/views/More.vue
@@ -2,7 +2,6 @@
 import AdItem from "../components/AdItem.vue";
 import JoinUs from "../components/JoinUs.vue";
 import AdvertiseWithUs from "../components/AdvertiseWithUs.vue";
-import Footer from "../components/Footer.vue";
 
 defineOptions({
   name: 'More'
@@ -18,7 +17,77 @@ defineOptions({
     </div>
     <div class="w-full md:w-3/5  p-4 text-center text-gray-200">
 
-  <JoinUs/>
+      <JoinUs/>
+
+      <div class="container relative z-10 mx-auto flex flex-wrap pt-4 pb-12">
+        <h2 class="w-full my-2 text-2xl font-bold leading-tight text-center text-purple-500">Soon in AUTOSHIFT</h2>
+
+        <div data-aos="fade-up" class="w-full mb-4">
+          <div class="h-1 mx-auto gradient w-64 opacity-25 my-0 py-0 rounded-t"></div>
+        </div>
+        <!--        Begin SDLC cards-->
+        <!--        SDLC card-->
+
+        <div data-aos="zoom-in-down" class="w-full md:w-1/4 p-6 flex flex-col flex-grow flex-shrink">
+          <div class="flex-1 bg-purple-100 rounded-t rounded-b-none overflow-hidden shadow">
+            <div class="flex items-center justify-start">
+              <button
+                  class="mx-4 lg:mx-4 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              >
+                Parts
+              </button>
+            </div>
+            <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+              <p class="text-gray-800 text-base px-6 mb-5">Understand Requirements</p>
+              <p class="text-gray-500 text-base px-6 mb-5">
+                Begin by gathering requirements to ensure the project aligns with your goals and needs. Clear
+                communication at this stage sets the foundation for a successful development process.
+              </p>
+            </a>
+          </div>
+        </div>
+        <!--        SDLC card-->
+
+        <div data-aos="zoom-in-down" class="w-full md:w-1/4 p-6 flex flex-col flex-grow flex-shrink">
+          <div class="flex-1 bg-purple-100 rounded-t rounded-b-none overflow-hidden shadow">
+            <div class="flex items-center justify-start">
+              <button
+                  class="mx-4 lg:mx-4 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              >
+                Services
+              </button>
+            </div>
+            <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+              <p class="text-gray-800 text-base px-6 mb-5">Craft Solutions</p>
+              <p class="text-gray-500 text-base px-6 mb-5">
+                Next, design the architecture and user interface, focusing on creating a seamless and intuitive
+                experience. Every element is planned for efficiency and scalability.
+              </p>
+            </a>
+          </div>
+        </div>
+        <!--        SDLC card-->
+
+        <div data-aos="zoom-in-down" class="w-full md:w-1/4 p-6 flex flex-col flex-grow flex-shrink">
+          <div class="flex-1 bg-purple-100 rounded-t rounded-b-none overflow-hidden shadow">
+            <div class="flex items-center justify-start">
+              <button
+                  class="mx-4 lg:mx-4 hover:underline gradient text-white font-bold rounded-full my-6 py-4 px-8 shadow-lg focus:outline-none focus:shadow-outline transform transition hover:scale-105 duration-300 ease-in-out"
+              >
+                Auctions
+              </button>
+            </div>
+            <a href="#" class="flex flex-wrap no-underline hover:no-underline">
+              <p class="text-gray-800 text-base px-6 mb-5">Build and Integrate</p>
+              <p class="text-gray-500 text-base px-6 mb-5">
+                Write clean, maintainable code, integrating various systems and ensuring everything works together
+                flawlessly. Security and performance are always top priorities.
+              </p>
+            </a>
+          </div>
+        </div>
+
+      </div>
 
 
       <AdvertiseWithUs/>
@@ -34,5 +103,7 @@ defineOptions({
 </template>
 
 <style scoped>
-
+.gradient {
+  @apply  bg-gradient-to-r from-[#ff80b5] to-[#9089fc]
+}
 </style>
