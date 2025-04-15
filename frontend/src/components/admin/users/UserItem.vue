7d44a75fe45e275cfab433f9ae5196dc1b621e1f
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-04-15 19:12:59 +0300

    adjust user item layout

--- frontend/src/components/admin/users/UserItem.vue
+++ frontend/src/components/admin/users/UserItem.vue
@@ -1,10 +1,13 @@
 <template>
   <div
-      class="flex items-center gap-3 p-2 cursor-pointer hover:bg-gray-100 rounded-lg transition"
+      class="flex items-center justify-between gap-3 p-2 cursor-pointer hover:bg-gray-100 rounded-lg transition"
       @click="goToProfile"
   >
-    <!-- User Avatar -->
-    <img :src="user.avatar" alt="User Avatar" class="w-16 h-16 rounded-full object-cover" />
+
+      <!-- Avatar -->
+      <span class="text-pink-500 font-lg font-extrabold">
+        <i class="fas fa-user text-3xl text-gray-400"></i>
+      </span>
 
       <!-- User Name -->
       <span class="text-pink-500 font-lg font-extrabold">{{ user.username }}</span>
