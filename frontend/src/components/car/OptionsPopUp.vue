e47a28ed95d045a378f08d4423dec174c7157005
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-04-23 21:23:21 +0300

    change props to objet type

--- frontend/src/components/car/OptionsPopUp.vue
+++ frontend/src/components/car/OptionsPopUp.vue
@@ -1,7 +1,8 @@
 <script setup lang="ts">
 
-import {PropType} from "vue";
 import OptionItem from "./OptionItem.vue";
+import {RefOption} from "./CarFormNew.vue";
+import {PropType} from "vue";
 
 const props = defineProps({
 
@@ -9,11 +10,16 @@ const props = defineProps({
     type: String,
     default: 'bottom', // Can be 'top', 'bottom', 'left', 'right'
   },
-  options: {
-    type: Array as PropType<string[]>,
-    required: true,
+  title: {
+    type: String,
+    required: false,
 
   },
+  options: {
+    type: Array as PropType<RefOption[]>,
+    required: true,
+  },
+
 });
 
 const emit = defineEmits(['choice', 'close']);
@@ -22,7 +28,7 @@ const emit = defineEmits(['choice', 'close']);
 // Emits close and save events
 
 const saveOption = (index: number) => {
-    emit('choice', props.options[index]);
+    emit('choice', props.options[index].name_en);
 };
 </script>
 
@@ -41,12 +47,12 @@ const saveOption = (index: number) => {
       <button @click="emit('close')" class="w-24 h-24 hover:bg-pink-500 flex items-center justify-center">
         <span class="fas fa-arrow-left text-white text-2xl"></span>
       </button>
-      <h1 class="text-white text-2xl text-center">Select an option</h1>
+      <h1 class="text-white text-2xl text-center">Select {{props.title}}</h1>
     </div>
 
     <div class=" flex-col grid grid-cols-1 overflow-y-scroll scrollbar-none ">
       <div  v-for="(opt, index) in options" :key="index">
-        <OptionItem :option="opt" class="w-full p-2 text-white hover:bg-pink-500" @click="saveOption(index)"></OptionItem>
+        <OptionItem :option="opt.name_en" class="w-full p-2 text-white hover:bg-pink-500" @click="saveOption(index)"></OptionItem>
       </div>
     </div>
 
