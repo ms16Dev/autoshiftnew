65ee1ade6f2270174b28f970011376f964acb0cc
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-05-01 21:09:47 +0300

    init static data pinia store at first load

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,12 @@
 <script setup lang="ts">
+import { onMounted } from 'vue'
+import {useStaticDataStore} from "./stores/staticDataStore.ts";
 
+const staticData = useStaticDataStore()
 
+onMounted(() => {
+  staticData.initialize()
+})
 </script>
 
 <template>
