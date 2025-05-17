b72c54371096087ac4a6079e5630884811ab15f1
Mohammed Sanabani <msanabani.dev@gmail.com>
2025-05-18 00:23:55 +0300

    minor ui changes

--- frontend/src/components/CarItem.vue
+++ frontend/src/components/CarItem.vue
@@ -3,7 +3,7 @@ import {computed, defineProps, withDefaults} from "vue";
 import type { CarListDto } from '../core/models/CarListDto';
 import {useStaticDataStore} from "../stores/staticDataStore.ts";
 import {config} from "../../config.ts";
-import {formatRelativeDate} from "../utils/dateUtils.ts";
+import {formatNumber, formatRelativeDate} from "../utils/dateUtils.ts";
 import {useI18n} from "vue-i18n";
 const { locale } = useI18n();
 
@@ -63,9 +63,9 @@ const formattedCreatedDate = computed(() =>
       <div class="w-full border-b-2 border-b-pink-700 "></div>
       <div class="flex flex-row w-full justify-between -translate-y-1/2">
 
-        <div class="z-10">
-          <div class="flex-box bg-pink-700 text-white font-bold text-sm px-2 h-[30px]">{{ staticData.getLocalizedName(staticData.getCurrencyById(car.currency)) }} {{ car.price }}</div>
-          <p class="w-fit bg-pink-400 text-white font-bold px-2 h-[30px]">{{ staticData.getLocalizedName(staticData.getStatusById(car.origin)) }}</p>
+        <div class="z-10 translate-y-1/2">
+          <div class="flex-box bg-pink-700 text-white font-bold text-sm px-2 h-[20px]">{{ staticData.getLocalizedName(staticData.getCurrencyById(car.currency)) }} {{ formatNumber(car.price) }}</div>
+          <p class="flex-box bg-pink-400 text-white text-sm font-bold px-2 h-[20px]">{{ staticData.getLocalizedName(staticData.getStatusById(car.origin)) }}</p>
         </div>
         <div class="absolute rounded-full ring-2 ring-pink-700 h-24 w-24 bg-gray-100 -translate-y-1/4 left-1/2 -translate-x-1/2 overflow-hidden">
           <img class="object-contain w-full h-full"
@@ -76,16 +76,16 @@ const formattedCreatedDate = computed(() =>
 
 
 
-        <div class="flex flex-col">
-        <div class="flex-box bg-pink-700 text-white font-bold text-lg px-2 h-[30px] ">{{staticData.getLocalizedName(staticData.getClassById(car.model))}}</div>
-        <div class="w-fit bg-pink-400 text-white font-bold px-2 h-[30px] self-end">{{ car.year }}</div>
+        <div class="flex flex-col translate-y-1/2">
+        <div class="flex-box bg-pink-700 text-white font-bold text-sm px-2 h-[20px] ">{{staticData.getLocalizedName(staticData.getClassById(car.model))}}</div>
+        <div class="w-fit bg-pink-400 text-white text-sm font-bold px-2 h-[20px] self-end">{{ car.year }}</div>
         </div>
 
       </div>
 
       <div class="flex flex-row xl:flex w-full justify-between px-3">
 
-        <div class="text-lg text-gray-900 text-center truncate w-full">
+        <div class="text-lg text-gray-900 text-center truncate w-full pt-4">
           {{ car.description ? car.description : 'No description added' }}
         </div>
 
